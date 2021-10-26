6d156b4d6b38c7a39f40ae8926ac9a96c432c25c
JAY-Kittisak <71874439+JAY-Kittisak@users.noreply.github.com>
2021-10-26 17:04:08 +0700

    ep.18 CR Code Scan  Finished.

---
+++ pages/reward/done.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <v-app-bar color="primary" dense flat dark>
+      <v-toolbar-title> Reward Card </v-toolbar-title>
+    </v-app-bar>
+    <v-container class="pt-0 pb-0">
+      <v-row>
+        <v-col cols="12">
+          <div class="mt-8 text-primary text-title text-center">
+            Finished.
+          </div>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
